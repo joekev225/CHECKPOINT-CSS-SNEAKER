08d31fd38ac57ca9a2a7a5df91533d14d136f6ae
joekev225 <ouragajoelkevin@gmail.com>
2023-02-20 17:47:53 +0000

    dom deuxieme commit

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 let logo=document.querySelector('.header-droite__logo-panier')
 logo.addEventListener('click', contenu)
 
-function contenu(){
-  let panier=document.querySelector('.panier-ouvert');
-  panier.style.display= 'block'  
 
-}
+function contenu(){
+  // let panier=document.querySelector('.panier-ouvert');
+  // panier.style.visibility= 'visible'  
+    let panier = document.querySelector('.panier-ouvert');
+    if (panier.style.visibility === 'visible') {
+      panier.style.visibility = 'hidden';
+    } else {
+      panier.style.visibility = 'visible';
+    }
+  }
+  
+
 
 
 
