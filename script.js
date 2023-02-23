d6bcd96564ed850f69587c494b6f20b162a1ae88
kouyate karim <kouyatekarim02@gmail.com>
2023-02-23 10:54:19 +0000

    fix

--- script.js
+++ script.js
@@ -2,17 +2,32 @@ let logo=document.querySelector('.header-droite__logo-panier')
 logo.addEventListener('click', contenu)
 
 
+window.addEventListener('click', closeCart);
+
 function contenu(){
   // let panier=document.querySelector('.panier-ouvert');
   // panier.style.visibility= 'visible'  
     let panier = document.querySelector('.panier-ouvert');
-    if (panier.style.visibility === 'visible') {
-      panier.style.visibility = 'hidden';
-    } else {
-      panier.style.visibility = 'visible';
-    }
+
+    // panier.classList.add('visible');
+    panier.classList.toggle('visible');
+    // if (panier.style.visibility === 'visible') {
+    //   panier.style.visibility = 'hidden';
+    // } else {
+    //   panier.style.visibility = 'visible';
+    // }
   }
   
+
+  function closeCart(event) {
+    let icon = document.querySelector('.panier_logo');
+    let panier = document.querySelector('.panier-ouvert');
+    if(event.target != icon){
+      panier.classList.remove('visible');
+    }
+
+    
+  }
 
 
 
