57d1e5065ac4025cfb5fb9cdf2908a4f06ed6dd3
joekev225 <ouragajoelkevin@gmail.com>
2023-02-26 22:37:11 +0000

    commit

--- script.js
+++ script.js
@@ -1,35 +1,47 @@
-let logo=document.querySelector('.header-droite__logo-panier')
-logo.addEventListener('click', contenu)
+// let logo=document.querySelector('.header-droite__logo-panier')
+// logo.addEventListener('click', contenu)
 
 
-window.addEventListener('click', closeCart);
+// window.addEventListener('click', closeCart);
+// function contenu(){
+//   // let panier=document.querySelector('.panier-ouvert');
+//   // panier.style.visibility= 'visible'  
+//     let panier = document.querySelector('.panier-ouvert');
 
-function contenu(){
-  // let panier=document.querySelector('.panier-ouvert');
-  // panier.style.visibility= 'visible'  
-    let panier = document.querySelector('.panier-ouvert');
-
-    // panier.classList.add('visible');
-    panier.classList.toggle('visible');
-    // if (panier.style.visibility === 'visible') {
-    //   panier.style.visibility = 'hidden';
-    // } else {
-    //   panier.style.visibility = 'visible';
-    // }
-  }
+//     // panier.classList.add('visible');
+//     panier.classList.toggle('visible');
+//     // if (panier.style.visibility === 'visible') {
+//     //   panier.style.visibility = 'hidden';
+//     // } else {
+//     //   panier.style.visibility = 'visible';
+//     // }
+//   }
   
 
-  function closeCart(event) {
-    let icon = document.querySelector('.panier_logo');
-    let panier = document.querySelector('.panier-ouvert');
-    if(event.target != icon){
-      panier.classList.remove('visible');
-    }
+//   function closeCart(event) {
+//     let icon = document.querySelector('.panier_logo');
+//     let panier = document.querySelector('.panier-ouvert');
+//     if(event.target != icon){
+//       panier.classList.remove('visible');
+//     }
 
     
-  }
+//   }
 
+let iconMenu = document.querySelector('.icon-menu');
+let menu = document.querySelector('.menu');
 
+iconMenu.addEventListener('click', afficherContenu);
 
+function afficherContenu() {
+menu.classList.toggle('menu-visible');
+}
+
+let closeIcone = document.querySelector('.delete')
+closeIcone.addEventListener('click', fermer)
+
+function fermer (){
+  document.querySelector('.menu-visible').classList.remove('menu-visible');
+}
 
 
